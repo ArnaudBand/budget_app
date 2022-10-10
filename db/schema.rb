c7e21c6c008d972e85d7fe4347fba56b5d0ffd30
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-10 10:03:48 -0600

    Migrate to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_154639) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_155432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_154639) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_groups_on_list_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -26,6 +30,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_154639) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.index ["group_id"], name: "index_lists_on_group_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_154639) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "lists"
+  add_foreign_key "groups", "users"
+  add_foreign_key "lists", "groups"
+  add_foreign_key "lists", "users"
 end
