0c813d8815026ea4630daef8bdf3d1cb931b8beb
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-11 23:22:14 -0600

    Generate a new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_171453) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_152945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "entities_groups", force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_entities_groups_on_entity_id"
+    t.index ["group_id"], name: "index_entities_groups_on_group_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -20,20 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_171453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.bigint "list_id"
-    t.index ["list_id"], name: "index_groups_on_list_id"
     t.index ["user_id"], name: "index_groups_on_user_id"
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.string "name"
-    t.integer "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "group_id"
-    t.index ["group_id"], name: "index_lists_on_group_id"
-    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,8 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_171453) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "groups", "lists"
+  add_foreign_key "entities", "users"
+  add_foreign_key "entities_groups", "entities"
+  add_foreign_key "entities_groups", "groups"
   add_foreign_key "groups", "users"
-  add_foreign_key "lists", "groups"
-  add_foreign_key "lists", "users"
 end
