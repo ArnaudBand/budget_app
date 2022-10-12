c807a85f3836dd1cb86455fe77ee61115b1bdc9a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-11 23:02:17 -0600

    Delete lists and groups table

--- app/models/list.rb
+++
@@ -1,7 +0,0 @@
-class List < ApplicationRecord
-  validates :name, presence: true
-  validates :amount, numericality: { greater_than: 0 }
-
-  belongs_to :user
-  has_and_belongs_to_many :groups
-end
