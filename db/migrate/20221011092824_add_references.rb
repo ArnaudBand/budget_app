bad62b37a0f17564aeb39c14e6a01f0a1f934a3d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-11 23:09:37 -0600

    Add references for user

---
+++ db/migrate/20221011092824_add_references.rb
@@ -0,0 +1,6 @@
+class AddReferences < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :entities, :user, foreign_key: true
+    add_reference :groups, :user, foreign_key: true
+  end
+end
