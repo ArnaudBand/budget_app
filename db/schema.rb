83d55cbb3709a3320ad299abac3232fbaa890a4b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-15 08:20:18 -0600

    Change user_id to author_id as required

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_152945) do
   create_table "entities", force: :cascade do |t|
     t.string "name"
     t.integer "amount", default: 0
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_entities_on_user_id"
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "entities_groups", force: :cascade do |t|
@@ -50,15 +50,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_152945) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "entities", "users"
+  add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "entities_groups", "entities"
   add_foreign_key "entities_groups", "groups"
   add_foreign_key "groups", "users"
