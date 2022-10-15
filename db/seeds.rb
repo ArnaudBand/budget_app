bfebdd603d8aa1b2903bca3b7241f08e151c55ec
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-15 02:51:13 -0600

    Config the capybara

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 # #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 # #   Character.create(name: "Luke", movie: movies.first)
 
-# User.create(name: 'Arnaud', password: '123456', email: 'arnaud@mail.com')
-
