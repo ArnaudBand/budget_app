593b0c4ae1cdb53f208d41e2850b3847fa796e6d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-15 02:47:24 -0600

    Give the user manage all and not others

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,10 @@
 class Ability
   include CanCan::Ability
+  def initilize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Group, user: user
+    can :manage, Entity, user:
+  end
 end
