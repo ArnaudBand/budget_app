40143da56bf6cb3413d03cf9e7bfefe6e7b88ede
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-15 07:31:10 -0600

    Fix the errors for confimable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  resources :users
 
   resources :groups, only: %i[create index new show] do
     resources :entities, only: %i[create index new show]
