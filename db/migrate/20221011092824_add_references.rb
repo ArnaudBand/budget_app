4852bba63b26abbd49868f44529a36ab66b8a3af
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-15 08:23:00 -0600

    Update the user_id to author_id

--- db/migrate/20221011092824_add_references.rb
+++ db/migrate/20221011092824_add_references.rb
@@ -1,6 +1,5 @@
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
-    add_reference :entities, :user, foreign_key: true
     add_reference :groups, :user, foreign_key: true
   end
 end
